12adc2d6ddab72df35c729abc4e5de0c6b54c130
ericbeechen <ebeechen@uchicago.edu>
2026-03-29 16:44:34 -0500

    Build Universe

--- MarketData.py
+++ MarketData.py
@@ -2,11 +2,36 @@ import os
 import pandas as pd
 import yfinance as yf
 import fredapi as Fred
-import datetime
 
 class MarketData:
     def __init__(self):
-        pass
+        self.securities = None
+        self.universe = None
+        self.returns = None
+
+    def pull_SP500_constituents(self):
+        sp500_table = pd.read_csv("https://datahub.io/core/s-and-p-500-companies/r/constituents.csv")
+        sp500_list = sp500_table["Symbol"].dropna().astype(str).str.replace(".", "-", regex=False).tolist()
+        self.securities = sp500_list
+        return self.securities
+
+    def build_universe(self, period: str = "1y", file_path: str | None = None):
+        output_file = file_path or f"yfinance_data/universe_{period}.xlsx"
+        if os.path.exists(output_file):
+            self.universe = pd.read_excel(output_file, index_col=0, parse_dates=True)
+            return self.universe
+        # Example S&P 500
+        self.pull_SP500_constituents()
+        # period can be 1d, 5d, 1mo, 3mo, 6mo, 1y, 2y, 5y, ytd, max
+        sp500_df = yf.download(self.securities, period=period, auto_adjust=False)["Adj Close"]
+        sp500_df.to_excel(output_file)
+        self.universe = sp500_df
+        return self.universe
+    
+    def universe_returns(self):
+        self.returns = self.universe.pct_change()[1:] # drop the first row which will be NaN due to the pct_change calculation
+        return self.returns
+
 
     def get_stock(self, ticker, start, end):
         stock_data = yf.download(ticker, start=start, end=end, auto_adjust=False)
--- PortfolioConstructor.py
+++ PortfolioConstructor.py
@@ -3,10 +3,24 @@ import ResearchViews
 import ClientProfile
 
 class PortfolioConstructor:
+    # This class will be responsible for constructing the portfolio based on market data, research views, and client profile.
+
     def __init__(self):
         self.market_data = MarketData.MarketData()
         self.research_views = ResearchViews.ResearchViews()
         self.client_profile = ClientProfile.ClientProfile()
 
     def construct_portfolio(self):
-        print('Constructing portfolio...')
+        print('Building universe of assets...')
+        universe = self.market_data.build_universe()
+        returns = self.market_data.universe_returns()
+        print(f'Universe of assets: {universe}')
+        print(f'Universe returns: {returns}')
+        # Step 1: Build Universe of Assets
+        # Step 2: Gather research views on the assets in the universe. e.g. which names or sectors to overweight or underweight, and which factors to tilt towards or away from.
+        # Step 3: Filter assets based on client profile and research views
+        # Step 4: Construct portfolio based on the above data
+
+
+portfolio = PortfolioConstructor()
+portfolio.construct_portfolio()
